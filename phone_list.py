579e4000cd130a30acf7daa3ccaf48a2ce3429e7
Momelezi <42340477+Momelezi@users.noreply.github.com>
2021-06-23 09:29:54 +0200

    Add files via upload

---
+++ phone_list.py
@@ -0,0 +1,12 @@
+phonelist = [
+  ['Meyers, Chris', '343-434', 'werty', '3652167@myuwc.ac.za9'],
+  ['Smith, Robert', 'amkeledanielmandumbu@gmail.com', 'ertyiuyt', '689-1234'],
+  ['Jones, Janet', 'anokrala@gmail.com', 'rghg', '483-5432'],
+  ['Barnhart, Ralph', '3652167@myuwc.ac.za','ihgtyhr', '683-2341'],
+  ['Nelson, Eric', 'amkeledanielmandumbu@gmail.com', 'uytr',  '485-2689'],
+  ['Prefect, Ford', 'anokrala@gmail.com', 'kjhgfr', '987-6543'],
+  ['Zigler, Mary', 'amkeledanielmandumbu@gmail.com', 'kjhgf', '567-8901'],
+  ['Smith, Bob', 'anokrala@gmail.com',  'kjhgfhg',  '689-1234']
+]
+
+
